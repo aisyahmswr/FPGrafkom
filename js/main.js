edd132d5d137f956b9ede8046d8cf6e863968900
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2018-12-12 00:10:22 +0700

    Add Custom Shader

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var scene, camera, renderer, loop;
 var cube, tunnel;
 var xSpeed = 0.2, ySpeed = 0.2;
 var moveRight = false, moveLeft = false, moveUp = false, moveDown = false;
+var start = Date.now();
 
 window.onload = function() {
     if (WEBGL.isWebGLAvailable()) {
@@ -32,10 +33,32 @@ function init() {
     var ambientLight = new THREE.AmbientLight();
     scene.add(ambientLight);
 
-   var cubeGeometry = new THREE.BoxGeometry();
+   var cubeGeometry = new THREE.BoxGeometry(1, 1);
    var cubeMaterial = new THREE.MeshStandardMaterial(); 
 
-   var tunnelGeometry = new THREE.CylinderGeometry(5, 5, 10, 20, 1, true);
+   var tunnelGeometry = new THREE.CylinderBufferGeometry(5, 5, 10, 20, 1, true);
+
+   var uniforms = {
+       delta: {value: 0},
+       time: {type: "f", value: 0.0}
+   }
+
+   var shaderMaterial = new THREE.ShaderMaterial({
+       //wireframe: true,
+       side: THREE.DoubleSide,
+       uniforms: uniforms,
+       vertexShader : document.getElementById('vertexShader').textContent,
+       fragmentShader : document.getElementById('fragmentShader').textContent
+   });
+
+   var vertexDisplacement = new Float32Array(tunnelGeometry.attributes.position.count);
+   for (var i = 0; i < vertexDisplacement.length; i += 1) {
+        vertexDisplacement[i] = Math.sin(i);
+   }
+
+   tunnelGeometry.addAttribute('vertexDisplacement', new THREE.BufferAttribute(vertexDisplacement, 1));
+
+   
    var tunnelMaterial = new THREE.MeshPhongMaterial({
        color: 0x4040f0,
        side: THREE.DoubleSide
@@ -44,7 +67,7 @@ function init() {
    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
    scene.add(cube);
 
-   tunnel = new THREE.Mesh(tunnelGeometry, tunnelMaterial);
+   tunnel = new THREE.Mesh(tunnelGeometry, shaderMaterial);
    scene.add(tunnel);
    tunnel.openEnded = true;
    tunnel.rotation.set(99, 0, 0);
@@ -60,7 +83,14 @@ function init() {
     renderer = new THREE.WebGLRenderer({canvas: document.getElementById('mainCanvas')});
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    var shaderDelta = 0;
+
     loop.add(function(delta){
+        shaderDelta += 0.1;
+        tunnel.material.uniforms.delta.value = 1 + Math.sin(shaderDelta) * 0.3;
+        tunnel.material.uniforms.time.value = 0.00025 * (Date.now()  - start);
+
         //cube.rotation.x += 0.1;
         if(moveUp && cube.position.y < 3.6) cube.position.y += ySpeed;
         if(moveDown && cube.position.y > -3.6) cube.position.y -= ySpeed;
@@ -69,7 +99,7 @@ function init() {
         if(moveRight  && cube.position.x < 3.6) cube.position.x += xSpeed;
 
         //console.log(cube.position.y);
-        tunnel.rotation.y += 0.05;
+        //tunnel.rotation.y += 0.05;
     
         renderer.render( scene, camera );
     });
