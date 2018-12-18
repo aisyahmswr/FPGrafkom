1017bdb22c3ab338e796b840e7dee85e7340ed45
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2018-12-18 19:21:10 +0700

    Added bubble

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var scene, camera, renderer, loop;
 var cube, tunnel;
-var xSpeed = 0.2, ySpeed = 0.2;
+var xSpeed = 0.1, ySpeed = 0.1;
 var moveRight = false, moveLeft = false, moveUp = false, moveDown = false;
 var start = Date.now();
+var vertexDisplacement;
+var particleGeometry, vertices = [];
 
 window.onload = function() {
     if (WEBGL.isWebGLAvailable()) {
@@ -36,7 +38,7 @@ function init() {
    var cubeGeometry = new THREE.BoxGeometry(1, 1);
    var cubeMaterial = new THREE.MeshStandardMaterial(); 
 
-   var tunnelGeometry = new THREE.CylinderBufferGeometry(5, 5, 10, 20, 1, true);
+   var tunnelGeometry = new THREE.CylinderBufferGeometry(10, 10, 25, 20, 25, true);
 
    var uniforms = {
        delta: {value: 0},
@@ -51,7 +53,7 @@ function init() {
        fragmentShader : document.getElementById('fragmentShader').textContent
    });
 
-   var vertexDisplacement = new Float32Array(tunnelGeometry.attributes.position.count);
+   vertexDisplacement = new Float32Array(tunnelGeometry.attributes.position.count);
    for (var i = 0; i < vertexDisplacement.length; i += 1) {
         vertexDisplacement[i] = Math.sin(i);
    }
@@ -70,10 +72,35 @@ function init() {
    tunnel = new THREE.Mesh(tunnelGeometry, shaderMaterial);
    scene.add(tunnel);
    tunnel.openEnded = true;
-   tunnel.rotation.set(99, 0, 0);
-   //tunnel.position.set(0, 0, 7.8);
+   tunnel.rotation.set(-99, 0, 0);
+   tunnel.position.set(0, 0, -8);
    console.log(tunnel);
-   
+
+   var textureLoader = new THREE.TextureLoader();
+
+   particleGeometry = new THREE.BufferGeometry();
+   var sprite = textureLoader.load('bubble.png');
+   vertices = [];
+
+   for ( var i = 0; i < 10000; i ++ ) {
+
+    var x = Math.random() * 2000 - 1000;
+    var y = Math.random() * 2000 - 1000;
+    var z = Math.random() * 2000 - 1000;
+
+    vertices.push( x, y, z );
+
+    }
+
+    particleGeometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+    particleMaterial = new THREE.PointsMaterial({
+        size: 10, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true
+    });
+    particleMaterial.color.setHSL( 0.9, 0, 0.5 );
+
+    var particles = new THREE.Points(particleGeometry, particleMaterial);
+    scene.add(particles);
 
     console.log("Ready to render");
     
@@ -87,8 +114,28 @@ function init() {
     var shaderDelta = 0;
 
     loop.add(function(delta){
+        var time = Date.now() * 0.00005;
         shaderDelta += 0.1;
-        tunnel.material.uniforms.delta.value = 1 + Math.sin(shaderDelta) * 0.3;
+
+        for ( var i = 0; i < scene.children.length; i ++ ) {
+
+            var object = scene.children[ i ];
+
+            if ( object instanceof THREE.Points ) {
+                console.log(object);
+                if(object.position.z > 300) object.position.set(THREE.Math.randFloatSpread(0.5), THREE.Math.randFloatSpread(0.5), 0);
+                else object.position.z += 10;
+                //object.rotation.z = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+            }
+
+        }
+        
+        for (var i = 0; i < vertexDisplacement.length; i += 1) {
+            vertexDisplacement[i] = Math.sin(i+shaderDelta);
+        }
+        tunnelGeometry.addAttribute('vertexDisplacement', new THREE.BufferAttribute(vertexDisplacement, 1));
+
+        tunnel.material.uniforms.delta.value = 0.5 +  Math.sin(shaderDelta) * 0.25;
         tunnel.material.uniforms.time.value = 0.00025 * (Date.now()  - start);
 
         //cube.rotation.x += 0.1;
@@ -99,7 +146,7 @@ function init() {
         if(moveRight  && cube.position.x < 3.6) cube.position.x += xSpeed;
 
         //console.log(cube.position.y);
-        //tunnel.rotation.y += 0.05;
+        //tunnel.rotation.y += 0.005;
     
         renderer.render( scene, camera );
     });
